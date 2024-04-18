ca34c6bc2d7d42bef65b172c225a18147c0a290f
usama913 <71500416+usama913@users.noreply.github.com>
2024-04-19 02:28:50 +0500

    Update IAS.ps1

--- IAS.ps1
+++ IAS.ps1
@@ -1,10 +1,10 @@
-# Check the instructions here on how to use it https://github.com/lstprjct/IDM-Activation-Script/wiki
+# Check the instructions here on how to use it https://github.com/usama913/IDM-Activation-Script/wiki
 
 $ErrorActionPreference = "Stop"
 # Enable TLSv1.2 for compatibility with older clients
 [Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12
 
-$DownloadURL = 'https://raw.githubusercontent.com/lstprjct/IDM-Activation-Script/main/IAS.cmd'
+$DownloadURL = 'https://raw.githubusercontent.com/usama913/IDM-Activation-Script/main/IAS.cmd'
 
 $rand = Get-Random -Maximum 99999999
 $isAdmin = [bool]([Security.Principal.WindowsIdentity]::GetCurrent().Groups -match 'S-1-5-32-544')
